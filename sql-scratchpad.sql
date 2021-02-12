159bd4ecfec4d0e3090d5d343098c148efffee9a
Kwame Taylor <maxkwametaylor@gmail.com>
2021-02-11 18:10:23 -0600

    Weather Observation Station 6

--- sql-scratchpad.sql
+++ sql-scratchpad.sql
@@ -1,2 +1,6 @@
 SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, CITY LIMIT 1;
 SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+-- Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY  RLIKE '^[aeiouAEIOU].*'
