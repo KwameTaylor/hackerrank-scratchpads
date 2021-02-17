1f9874c9e302cb754676834d4020125167d05af0
Kwame Taylor <maxkwametaylor@gmail.com>
2021-02-17 13:20:19 -0600

    more solved

--- sql-scratchpad.sql
+++ sql-scratchpad.sql
@@ -19,3 +19,12 @@ SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[aeiouAEIOU].*.[aeiouAEIOU]
 -- Query the list of CITY names from STATION that do not start
 -- with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE CITY NOT RLIKE '^[aeiouAEIOU].*';
+
+-- Query the list of CITY names from STATION that do not end
+-- with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT RLIKE '^*.[aeiouAEIOU]$';
+
+-- Query the list of CITY names from STATION that do not
+-- start with vowels and do not end with vowels. Your
+-- result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT RLIKE '^[aeiouAEIOU].*|.*[AEIOUaeiou]$';
