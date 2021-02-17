9cfbd0b836de648093736a92fdf908c27a80342a
Kwame Taylor <maxkwametaylor@gmail.com>
2021-02-17 12:10:35 -0600

    solve vowel exercise

--- sql-scratchpad.sql
+++ sql-scratchpad.sql
@@ -10,3 +10,8 @@ SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[aeiouAEIOU].*';
 -- Query the list of CITY names ending with vowels (a, e, i, o, u)
 -- from STATION. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^*.[aeiouAEIOU]$';
+
+-- Query the list of CITY names from STATION which have vowels
+-- (i.e., a, e, i, o, and u) as both their first and last characters.
+-- Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[aeiouAEIOU].*.[aeiouAEIOU]$';
